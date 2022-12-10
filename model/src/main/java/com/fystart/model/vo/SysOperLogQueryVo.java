b43c0259cb02e103fb464643ddef925f29cc3eaf
fy <fystart.163.com>
2022-12-10 13:57:30 +0800

    初始化项目

---
+++ model/src/main/java/com/fystart/model/vo/SysOperLogQueryVo.java
@@ -0,0 +1,15 @@
+package com.fystart.model.vo;
+
+import lombok.Data;
+
+@Data
+public class SysOperLogQueryVo {
+
+	private String title;
+	private String operName;
+
+	private String createTimeBegin;
+	private String createTimeEnd;
+
+}
+
